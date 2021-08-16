f15152b413241344b60d4f17f9405e0692a48f1f
Kinsey Andrea Nietzsche <kinsey.nietzsche@gmail.com>
2021-08-16 10:15:15 +0200

    preparing the project

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,18 @@
+﻿public static class GameSettings
+{
+    public const int CellCountX = 7;
+    public const int CellCountY = 6;
+
+    public const float CellSizeX = 162f;
+    public const float CellSizeY = 186f;
+    
+    public const int CellTypeCount = 4;
+    public const float durationAnimation = .8f;
+
+    public const string dataKeyScore = "score";
+    public const string dataKeyTurn = "round";
+
+    public const int turnMax = 20;
+    public const int scoreCell = 100;
+    public const int scoreBonusMultiplier = 2;
+}
